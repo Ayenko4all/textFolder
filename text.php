b505fdb261a30f1139b92851c65c6043d16bacf5
Ayenko4all <kenny4all40@gmail.com>
2019-09-18 03:02:48 +0100

    add

---
+++ text.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Tech Test</title>
+</head>
+<body>
+  <h1>MY FIRST COMMIT</h1>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil facere vitae dignissimos libero culpa eaque, fuga eius voluptates animi dolore ullam eligendi quo reprehenderit inventore voluptas architecto sint delectus atque!
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora, eveniet. Sunt assumenda veniam itaque magni nulla voluptates incidunt tempora tenetur quae deleniti quia a, eaque repudiandae dolores iusto officiis quas?
+  </p>
+</body>
+</html>
+
+<?php  ;?>
